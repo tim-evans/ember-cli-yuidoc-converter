203823a3dc7fc9c01741c50ed77e0d981dc62054
Trek Glowacki <trek@groupon.com>
2015-01-01 17:15:16 -0600

    Use option parsing over environment variables

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 'use strict';
 
 var fs = require('fs');
+var nopt = require("nopt");
+
+var opts = {
+  "lib-dir" : String,
+  "rev": String,
+  "sha": [String, null]
+}
 var DocTree = require('./addon');
 
-var libDir = process.env.YUIDOC_LIB_DIR;
+var parsedOptions = nopt(opts);
+
+var libDir = parsedOptions['lib-dir']
 if (!libDir) {
-  throw new Error("API builder excepted environment variable YUIDOC_LIB_DIR. It was not set");
+  throw new Error("API builder cli argumemnt --lib-dir=/some/lib/path. It was not set");
 }
 
 var stat = fs.lstatSync(libDir);
 if (!stat.isDirectory()) {
   throw new Error(
-    "API builder excepted environment variable YUIDOC_LIB_DIR to reference a directory. `" +
+    "API builder excepted cli argumemnt --lib-dir=/some/lib/path to reference a directory. `" +
     libDir +
     "` is not a directory"
   );
